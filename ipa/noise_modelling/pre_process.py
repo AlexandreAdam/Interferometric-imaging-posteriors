fb005495eeb841db42dd59a9ed28ab64bc91db9e
mb010 <micah.bowles@postgrad.manchester.ac.uk>
2023-06-20 16:24:21 +0100

    Noise simulation using casa 6.2

--- ipa/noise_modelling/pre_process.py
+++ ipa/noise_modelling/pre_process.py
@@ -1,5 +1,5 @@
 from casatools import table, ms
-from casatasks import mstransform, split, tclean
+from casatasks import mstransform
 from pathlib import Path
 import argparse
 import os
@@ -9,80 +9,6 @@ import numpy as np
 
 from casatools import table
 import numpy as np
-
-
-def estimate_system_temperature(ms_path):
-    # Create a table tool
-    tb_tool = table()
-
-    # Open the MS
-    tb_tool.open(ms_path)
-
-    # Get the time, antenna1, antenna2, and visibility data
-    time = tb_tool.getcol("TIME")
-    antenna1 = tb_tool.getcol("ANTENNA1")
-    antenna2 = tb_tool.getcol("ANTENNA2")
-    data = tb_tool.getcol("DATA")
-
-    # Close the MS
-    tb_tool.close()
-
-    # Get the unique baselines
-    baselines = set(zip(antenna1, antenna2))
-
-    diff_over_time = []
-
-    # For each baseline, calculate the diff over time
-    for baseline in baselines:
-        # Find the indices for this baseline
-        indices = np.where((antenna1 == baseline[0]) & (antenna2 == baseline[1]))
-
-        # Sort the indices by time
-        sorted_indices = indices[0][np.argsort(time[indices])]
-
-        # Get the data for this baseline, sorted by time
-        sorted_data = data[sorted_indices]
-
-        # Calculate the diff over time
-        diff = np.diff(sorted_data, axis=0)
-
-        diff_over_time.append(diff)
-
-    return diff_over_time.std() / 2**0.5
-
-
-def overwrite_visibilities(ms_path, new_ms_path, new_visibilities=None, sigma_n=None):
-    # Create a copy of the original MS
-    split(vis=ms_path, outputvis=new_ms_path, datacolumn="data")
-
-    # Create a table tool
-    tb_tool = table()
-
-    # Open the new MS
-    tb_tool.open(new_ms_path, nomodify=False)
-
-    # Get the existing visibilities
-    existing_visibilities = tb_tool.getcol("DATA")
-
-    # estimate noise or use predefined visibilities
-    if sigma_n is not None:
-        real = np.random.normal(0, sigma_n, existing_visibilities.shape)
-        imaginary = 1j * np.random.normal(0, sigma_n, existing_visibilities.shape)
-        new_visibilities = real + imaginary
-
-    else:
-        pass
-    # Check the shapes of the existing and new visibilities
-    if existing_visibilities.shape != new_visibilities.shape:
-        raise ValueError(
-            "Shape of new visibilities does not match existing visibilities"
-        )
-
-    # Overwrite the visibilities
-    tb_tool.putcol("DATA", new_visibilities)
-
-    # Close the MS
-    tb_tool.close()
 
 
 def find_largest_spw(ms_path):
@@ -118,18 +44,6 @@ def save_largest_spw(ms_path, new_ms_path):
         split(
             vis=ms_path, outputvis=new_ms_path, spw=str(largest_spw), datacolumn="data"
         )
-
-
-def quick_clean(vis):
-    vis = Path(vis)
-    tclean(
-        str(vis),
-        imagename=str(vis.root + vis.stem),
-        datacolumn="data",
-        imsize=(512, 512),
-        cell=["0.003arcsec", "0.003arcsec"],
-        niter=0,
-    )
 
 
 def ensure_single_spectralwindow(ms_path, outname=None):
@@ -209,6 +123,3 @@ if __name__ == "__main__":
         outname = args.outname
     if Path(outname).is_dir == False:
         save_largest_spw(str(ms_file), str(outname))
-    else:
-        quick_clean(vis=str(ms_file))
-        quick_clean(vis=str(outname))
--- ipa/noise_modelling/runnoiseim.sh
+++ ipa/noise_modelling/runnoiseim.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
 #SBATCH --time=15:00:00
+#SBATCH --mem=800G
 #SBATCH --constraint=A100
 #SBATCH --output=logs/%j.%x.out
 #SBATCH --exclude=compute-0-4
 #SBATCH --job-name=NoiseSim
 #SBATCH --mail-user=micah.bowles@postgrad.manchester.ac.uk
 
-# Parameterise the docker container.
-# Use the gain cal, phase, [...], error generation built into oskar.
-echo "hello world"
-echo getopts
+# Help description of the shell script
+SCRIPT_NAME="$(basename "$0")"
+display_help() {
+  cat <<EOF
+Usage: $SCRIPT_NAME [options] [arguments]
 
+Description: Use a measurement set to generate a number of 
+    measurement sets which have false data but the same 
+    configuration. Currently fills data with thermal noise 
+    estimated from the original data. The data is saved to 
+    a 'data' folder in the current directory.
+
+Options:
+  -h, --help     Display this help message and exit.
+  -n             Number of samples to generate.
+  -p             Path to the measurement used as base.
+  ...
+EOF
+}
+
+# Read in command line options
 while getopts ":n:p:" opt; do
   case $opt in
-    n) NO_SAMPLES="$OPTARG"
+  n)
+    NO_SAMPLES="$OPTARG"
     ;;
-    p) PATH_TO_MS="$OPTARG"
+  p)
+    PATH_TO_MS="$OPTARG"
     ;;
-    \?) echo "Invalid option -$OPTARG" >&2
+  \?)
+    echo ">>> Invalid option -$OPTARG" >&2
     exit 1
     ;;
   esac
 
   case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
+  -*)
+    echo ">>> Option $opt needs a valid argument"
     exit 1
     ;;
   esac
@@ -34,42 +55,28 @@ done
 if ! [[ $NO_SAMPLES =~ ^-?[0-9]+$ ]]; then
   N=$(expr "$NO_SAMPLES" + 0 2>/dev/null)
   if [ $? -ne 0 ]; then
-    echo "Error: -n must be an integer. Setting to 1." >&2
+    echo ">>> Error: -n must be an integer. Setting to 1." >&2
     NO_SAMPLES=1
   fi
 fi
 
-runoskar2() {
-    singularity exec --bind /share/nas2_5/ --nv /share/nas2_5/mbowles/data/OSKAR-2.8.2-Python3.sif $1 $2
-}
-
-runoskarpy2() {
-    singularity exec --bind /share/nas2_5/ --nv /share/nas2_5/mbowles/data/OSKAR-2.8.2-Python3.sif python3 $1 $2
-}
-
+# Make necessary output folders
+CURRENT_PATH=$(pwd)
 mkdir -p data
+FOLDER_NAME=$(basename "$PATH_TO_MS")
+FOLDER_NAME="${FOLDER_NAME%.*}"
+OUT_PATH=$CURRENT_PATH/data/$FOLDER_NAME/
+echo ">>> Creating $OUT_PATH folder for output."
+mkdir -p $OUT_PATH
 
-ls /share/nas2_5/mbowles/data/
-
-echo ">>> start"
+echo ">>> Activating environment"
 source /home/mbowles/.bashrc # Replace with my own.
 source activate conda_casa
 
-# Pre process data
-directory=$(dirname "$PATH_TO_MS")
-filename=$(basename "$PATH_TO_MS")
-extension="${filename##*.}"
-filename="${filename%.*}"
-new_filename="${filename}_processed.${extension}" # File name change. data.ms -> data_processed.ms
-PROCESSED_DATA="${directory}/${new_filename}"
-python pre_process.py $PATH_TO_MS -o $PROCESSED_DATA
-
-echo ">>> run oskar"
-for ((i=0; i<$NO_SAMPLES; i++)); do
-  echo "Starting the $i'th run"
-  # python3 gennoise.py # Generate a random noise file "../visdata/noise_temp"
-  runoskarpy2 writevis.py $PROCESSED_DATA # Write a visibility set based on a measurement set and the generated noise_temp file
-  runoskarpy2 noiseim.py $i # Image the generated visibility.
-done
+echo ">>> Starting noise modelling."
+python noise_modelling.py \
+  --ms $PATH_TO_MS \
+  --outpath $OUT_PATH \
+  --number $NO_SAMPLES
 
 echo ">>> finish"
